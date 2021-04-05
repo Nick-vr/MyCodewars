5be2e004caece80600323ab60b3d54b26b66f797
Nick-vr <nvanroyen@gmail.com>
2021-04-05 13:28:00 +0200

    Typo

--- Codewars/Codewars.cs
+++ Codewars/Codewars.cs
@@ -40,8 +40,8 @@ namespace Codewars
             //Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
             // Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
             // Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
-            var twentyfive = 0; 1
-            var fifty = 0; 1
+            var twentyfive = 0;
+            var fifty = 0;
             var answer = "";
 
             Console.WriteLine("[{0}]", string.Join(", ", peopleInLine));
